e25d5df864bd03b48b4f1d2fda0e6b8ddcf297e7
Matt McCarthy <mmccarthy10@aurora.edu>
2019-09-19 12:42:14 -0500

    Split code into multiple files

---
+++ car.py
@@ -0,0 +1,11 @@
+# Constructor by Matt McCarthy
+class Car:
+    speed = 0
+    started = False
+
+    def start(self):
+        self.started = True
+
+    def isStarted(self):
+        return self.started
+
